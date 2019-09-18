7cff1bba3c97c8578146b70f3990628ddbc2c727
Sascha Timme <sascha.timme@gmail.com>
2019-09-18 18:02:33 +0200

    More improvements

--- src/HomotopyContinuationGym.jl
+++ src/HomotopyContinuationGym.jl
@@ -16,13 +16,16 @@ struct PathInfo
     s::Vector{Float64}
     Δs::Vector{Float64}
     ω::Vector{Float64}
+    Δx₀::Vector{Float64}
     accepted_rejected::Vector{Bool}
     norm_x::Vector{Float64}
     cond::Vector{Float64}
     accuracy::Vector{Float64}
     residual::Vector{Float64}
+    limit_accuracy::Vector{Float64}
     eval_err::Vector{Float64}
     # total info
+    return_code::HC.CoreTrackerStatus
     n_factorizations::Int
     n_ldivs::Int
 end
@@ -33,10 +36,12 @@ function path_info(tracker::HC.CoreTracker, x₀, t₁, t₀)
     s = Float64[]
     Δs = Float64[]
     ω = Float64[]
+    Δx₀ = Float64[]
     accepted_rejected = Bool[]
     norm_x = Float64[]
     cond = Float64[]
     accuracy = Float64[]
+    limit_accuracy = Float64[]
     residual = Float64[]
     eval_err = Float64[]
 
@@ -44,9 +49,11 @@ function path_info(tracker::HC.CoreTracker, x₀, t₁, t₀)
     push!(s, state.s)
     push!(Δs, state.Δs)
     push!(ω, state.ω)
+    push!(Δx₀, state.norm_Δx₀)
     push!(norm_x, maximum(abs, state.x))
     push!(cond, state.jacobian.cond[])
     push!(accuracy, state.accuracy)
+    push!(limit_accuracy, state.limit_accuracy)
     push!(residual, maximum(state.residual))
     push!(eval_err, maximum(state.eval_error))
 
@@ -56,9 +63,11 @@ function path_info(tracker::HC.CoreTracker, x₀, t₁, t₀)
         push!(s, state.s)
         push!(Δs, state.Δs)
         push!(ω, state.ω)
+        push!(Δx₀, state.norm_Δx₀)
         push!(norm_x, maximum(abs, state.x))
         push!(cond, state.jacobian.cond[])
         push!(accuracy, state.accuracy)
+        push!(limit_accuracy, state.limit_accuracy)
         push!(residual, maximum(state.residual))
         push!(eval_err, maximum(state.eval_error))
         first = false
@@ -69,12 +78,15 @@ function path_info(tracker::HC.CoreTracker, x₀, t₁, t₀)
         s,
         Δs,
         ω,
+        Δx₀,
         accepted_rejected,
         norm_x,
         cond,
         accuracy,
         residual,
+        limit_accuracy,
         eval_err,
+        HC.status(tracker),
         state.jacobian.factorizations[],
         state.jacobian.ldivs[],
     )
@@ -82,7 +94,7 @@ end
 
 path_table(info::PathInfo) = path_table(stdout, info)
 function path_table(io::IO, info::PathInfo)
-    header = ["", "s", "Δs", "ω", "acc", "κ", "ψ", "||x||", "||r||"]
+    header = ["", "s", "Δs", "ω", "|Δx₀|", "acc", "κ", "ψ", "limit_acc", "|x|", "|r|"]
     h1 = Highlighter(f = (data, i, j) -> j == 1 && data[i, 1] == :✗, crayon = crayon"red")
     h2 = Highlighter(f = (data, i, j) -> j == 1 && data[i, 1] == :✓, crayon = crayon"green")
     ✓✗ = map(v -> v ? :✓ : :✗, info.accepted_rejected)
@@ -91,17 +103,20 @@ function path_table(io::IO, info::PathInfo)
         round.(info.s, sigdigits = 3),
         round.(info.Δs, sigdigits = 3),
         round.(info.ω, sigdigits = 3),
-        round.(info.accuracy, sigdigits = 3),
+        round.(info.Δx₀, sigdigits = 2),
+        round.(info.accuracy, sigdigits = 2),
         round.(info.cond, sigdigits = 3),
-        round.(info.eval_err, sigdigits = 3),
+        round.(info.eval_err, sigdigits = 2),
+        round.(info.limit_accuracy, sigdigits = 2),
         round.(info.norm_x, sigdigits = 3),
-        round.(info.residual, sigdigits = 3),
+        round.(info.residual, sigdigits = 2),
     )
     pretty_table(io, data, header, crop = :none, highlighters = (h1, h2))
 end
 
 function Base.show(io::IO, info::PathInfo)
     println(io, "PathInfo:")
+    println(io, " • # return code → ", info.return_code)
     println(
         io,
         " • # steps (✓/✗) → ",
@@ -116,6 +131,5 @@ function Base.show(io::IO, info::PathInfo)
     println(io, " • # ldivs → ", info.n_ldivs)
     path_table(io, info)
 end
-
 
 end

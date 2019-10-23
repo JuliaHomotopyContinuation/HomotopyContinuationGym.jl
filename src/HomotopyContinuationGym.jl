f1023402d07306fff11fecd4b67567c0f64d2ace
Sascha Timme <sascha.timme@gmail.com>
2019-10-23 18:19:18 +0200

    Work better with HC.jl

--- src/HomotopyContinuationGym.jl
+++ src/HomotopyContinuationGym.jl
@@ -1,14 +1,24 @@
 module HomotopyContinuationGym
 
-export CTPathInfo, path_info
-
-using DelimitedFiles, LinearAlgebra, PrettyTables, Random, Printf
+using DelimitedFiles, LinearAlgebra, Random
 
 import DynamicPolynomials
 const DP = DynamicPolynomials
 import HomotopyContinuation
 const HC = HomotopyContinuation
 
+import HomotopyContinuation: parameters, nvariables, npolynomials, nparameters, solve
+
 include("systems.jl")
+
+function solve(sys::TestSystem; kwargs...)
+    solve(
+        system(sys),
+        start_solutions(sys);
+        parameters = parameters(sys),
+        start_parameters = start_parameters(sys),
+        kwargs...,
+    )
+end
 
 end
